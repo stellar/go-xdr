cc2b0611aabfce2351b1a877ad944278ea0febf0
tamirms <tamirms@gmail.com>
2026-03-12 17:58:20 -0500

    decoder: add MaxMemoryBytes decode option (#22)
    * decoder: add MaxOutputBytes option for cumulative decoded output size limits
    
    Add a MaxMemoryBytes field to DecodeOptions that tracks cumulative
    decoded memory allocatoins across a single decode operation. Before each
    allocation (array element, union arm, optional field, opaque data),
    the size is added to a running total; if it exceeds MaxOutputBytes
    the decode is aborted with ErrOutputBytesExceeded.

--- gotest.sh
+++ gotest.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # The script does automatic checking on a Go package and its sub-packages, including:
 # 1. goimports     (https://golang.org/x/tools/cmd/goimports)
-# 2. golint        (https://github.com/golang/lint)
-# 3. go vet        (https://golang.org/cmd/vet)
-# 4. test coverage (https://blog.golang.org/cover)
+# 2. go vet        (https://golang.org/cmd/vet)
+# 3. test coverage (https://blog.golang.org/cover)
 
 set -ex
 
 test -z "$(goimports -l -w .)"
-test -z "$(golint ./...     )"
 go vet ./...
 go test -covermode=atomic -race ./...
